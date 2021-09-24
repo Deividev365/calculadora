c6b8888df92321bace38ed1a821adf47ce6d5e10
Deivid Almeida <deividalmeida365@gmail.com>
2021-09-24 20:21:54 -0300

    new features

--- app.c
+++ app.c
@@ -3,5 +3,26 @@
 
 
 int main() {
-    printf("Teste");
+
+
+
+  char operador;
+  double primeiro, segundo;
+  printf("Entre um operador (...operadores)");
+  scanf("%c", &operador);
+
+  printf("Entre o primeiro operador");
+  scanf("%lf", &primeiro);
+
+  printf("Entre o segundo operador");
+  scanf("%lf", &segundo);
+
+  switch (operador) {
+    case '+':
+      printf("%.1lf + %.1lf = %.1lf", primeiro, segundo, primeiro + segundo);
+      break;
+
+  }
+
+  return 0;
 }
