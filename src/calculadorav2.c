05fb872a5e485cd1360fcd29fab798b7ac41b625
Deivid Almeida <deividalmeida365@gmail.com>
2021-10-19 21:57:27 -0300

    added version2 calc

---
+++ src/calculadorav2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int soma(int x, int y) {
+    return x + y;
+}
+
+int main() {
+
+    /*Cada um irá realizar uma funcionalidade, para concretizar a utilização da metodologia FDD - Feature Driven Develpment*/
+
+
+
+    /*
+    Irina irá desenvolver: nome
+    
+    
+    */
+
+   /*
+    Deivid irá desenvolver: nome
+    
+    
+    */
+
+   /*
+    Bruna irá desenvolver: nome
+    
+    
+    */
+
+   /*
+    Beatriz irá desenvolver: nome
+    
+    
+    */
+}
